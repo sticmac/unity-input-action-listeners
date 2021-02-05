0360bfe3742cf7034e9ac7a45e56f9565a864837
Julien Lemaire <julien.lemaire@sticmac.fr>
2021-02-05 23:13:41 +0100

    Base fields in Input Listener component

---
+++ Runtime/InputListener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace Sticmac.InputHandler {
+    public class InputListener : MonoBehaviour
+    {
+        [SerializeField] PlayerInput _playerInput = null;
+
+        [SerializeField] string _selectedActionName = null;
+
+        public UnityEvent Started;
+        public UnityEvent Performed;
+        public UnityEvent Canceled;
+
+        
+    }
+}
