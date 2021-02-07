b6065395a43c0b11037a5ffd6f899a027d9edbaf
Julien Lemaire <julien.lemaire@sticmac.fr>
2021-02-07 20:20:06 +0100

    Move some of the code in abstract mother class

---
+++ Runtime/AbstractInputListener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Sticmac.InputHandler {
+    public abstract class AbstractInputListener : MonoBehaviour {
+        /// <summary>
+        /// Player Input component for being plugged to the event system
+        /// </summary>
+        [SerializeField] protected PlayerInput _playerInput = null;
+
+        [SerializeField] protected string _selectedActionName = null;
+
+        protected virtual void OnEnable() {
+            _playerInput.notificationBehavior = PlayerNotifications.InvokeCSharpEvents;
+            _playerInput.onActionTriggered += HandleInput;
+        }
+        
+        protected virtual void OnDisable() {
+            _playerInput.onActionTriggered -= HandleInput;
+        }
+
+        protected virtual void Reset() {
+            _playerInput = GetComponent<PlayerInput>();
+        }
+
+        /// <summary>
+        /// Catches an emitted input from the player and triggers the corresponding callback if necessary
+        /// </summary>
+        /// <param name="context">The whole context of the player input</param>
+        protected abstract void HandleInput(InputAction.CallbackContext context);
+    }
+}
--- Runtime/InputListener.cs
+++ Runtime/InputListener.cs
@@ -1,44 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 namespace Sticmac.InputHandler {
-    public class InputListener : MonoBehaviour
+    public class InputListener : AbstractInputListener
     {
-        [SerializeField] PlayerInput _playerInput = null;
-
-        [SerializeField] string _selectedActionName = null;
-
+        /// <summary>
+        /// Event to be called when the action started
+        /// </summary>
         public UnityEvent Started;
         public UnityEvent Performed;
         public UnityEvent Canceled;
-
-        private void OnEnable() {
-            _playerInput.notificationBehavior = PlayerNotifications.InvokeCSharpEvents;
-            _playerInput.onActionTriggered += HandleInput;
-        }
-        
-        private void OnDisable() {
-            _playerInput.onActionTriggered -= HandleInput;
-        }
-
-        private void Reset() {
-            _playerInput = GetComponent<PlayerInput>();
-        }
 
         /// <summary>
         /// Catches an emitted input from the player and triggers the corresponding callback if necessary
         /// </summary>
         /// <param name="context">The whole context of the player input</param>
-        private void HandleInput(InputAction.CallbackContext context) {
+        protected override void HandleInput(InputAction.CallbackContext context) {
             if (_selectedActionName == context.action.name) {
                 if (context.started) {
                     Started.Invoke();
-                } else if (context.performed) {
+                }
+                if (context.performed) {
                     Performed.Invoke();
-                } else if (context.canceled) {
+                }
+                if (context.canceled) {
                     Canceled.Invoke();
                 }
             }
