4812c94eff18f0e8b64a9d8f9749ea2feff54fd4
Julien Lemaire <julien.lemaire@sticmac.fr>
2021-02-12 18:09:52 +0100

    Float input action listener and test

---
+++ Runtime/Parametrized/FloatInputActionListener.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace Sticmac.InputActionListeners {
+    [Serializable]
+    public class FloatUnityEvent : UnityEvent<float> {}
+
+    public class FloatInputActionListener : ParametrizedInputActionListener<float, FloatUnityEvent> {}
+}
