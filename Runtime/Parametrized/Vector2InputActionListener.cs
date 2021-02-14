a5147096815436ed8939d3f9142e41b9f2b67fc8
Julien Lemaire <julien.lemaire@sticmac.fr>
2021-02-14 21:55:43 +0100

    Vector2 Input Action Listener

---
+++ Runtime/Parametrized/Vector2InputActionListener.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Sticmac.InputActionListeners {
+    public class Vector2InputActionListener : ParametrizedInputActionListener<Vector2, Vector2InputActionListener.UnityEvent> {
+        [Serializable]
+        public class UnityEvent : UnityEvent<Vector2> {}
+    }
+}
---
+++ Tests/Runtime/Parametrized/Vector2InputActionListenerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.InputSystem;
+using System.Collections;
+using NUnit.Framework.Constraints;
+
+namespace Sticmac.InputActionListeners {
+    public class Vector2InputActionListenerTests : ParametrizedInputActionListenerTests<Vector2, Vector2InputActionListener.UnityEvent, Vector2InputActionListener>
+    {
+        protected override InputAction CreateSelectedAction() =>
+            _actionMap.AddAction(SelectedActionName, binding: "<Gamepad>/leftStick");
+
+        public override void TriggerSelectedAction() => Set(_gamepad.leftStick, new Vector2(0.5f, 0.5f));
+        public override void CancelSelectedAction() => Set(_gamepad.leftStick, Vector2.zero);
+        public override IResolveConstraint IsValid() => Is.EqualTo(new Vector2(0.5f, 0.5f))
+            .Using((Vector2 v1, Vector2 v2) => { // comparison through magnitude
+                float distance = Vector2.Distance(v1, v2);
+                if (distance <= 0.05f) {
+                    return 0;
+                } else {
+                    return 1; // Comparison between vectors makes non sense, so we just put 1 to mark the difference
+                }
+            });
+    }
+}
